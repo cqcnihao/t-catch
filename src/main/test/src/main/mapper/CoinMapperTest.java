4b7eb93ef16769282667088dcafdf6dabe691451
panbenxing <panbenxing@jd.com>
2019-04-02 18:48:33 +0800

    todo

--- src/main/test/src/main/mapper/CoinMapperTest.java
+++ src/main/test/src/main/mapper/CoinMapperTest.java
@@ -1,19 +1,9 @@
 package src.main.mapper;
 
-import com.alibaba.fastjson.JSON;
 import com.git.poan.trade.ServerMain;
-import com.git.poan.trade.entity.YieldCoin;
-import com.git.poan.trade.mapper.YieldCoinMapper;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @Author: panbenxing
@@ -24,12 +14,4 @@ import java.util.List;
 @SpringBootTest(classes = ServerMain.class)
 public class CoinMapperTest {
 
-    @Resource
-    private YieldCoinMapper yieldCoinMapper;
-
-    @Test
-    public void test() {
-        List<YieldCoin> yieldCoins = yieldCoinMapper.selectAll();
-        System.out.println(JSON.toJSONString(yieldCoins));
-    }
 }
